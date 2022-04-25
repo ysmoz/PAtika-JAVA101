b6709885849ec2089e7efec123d9053423e4c28c
Yesim Oz <yes.1995@hotmail.com>
2022-04-25 17:54:39 +0300

    create ucgenin alanı bulma

---
+++ src/DaireAlan.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DaireAlan {
+    public static void main(String[] args) {
+
+        int r;
+        double pi=3.14,alan,a;
+        Scanner inp= new Scanner(System.in);
+        System.out.print("dairenin yarıçapını giriniz:");
+        r=inp.nextInt();
+        System.out.print("dairenin merkez açı ölçusunu giriniz:");
+        a=inp.nextInt();
+
+        alan=(pi * (r*r) * a) / 360;
+        System.out.println(" DAirenin alanı:"+alan);
+
+
+    }
+}
