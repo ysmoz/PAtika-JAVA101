cb019ab4af72071616e396008072a5867a8585dd
Yesim Oz <yes.1995@hotmail.com>
2022-04-29 22:15:14 +0300

    create EnBuyukEnKucukBulma

---
+++ src/MukemmelSayi.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+public class MukemmelSayi {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        int girilenSayi;
+        int toplam=0;
+
+
+        System.out.println("Sayi giriniz: ");
+        girilenSayi = inp.nextInt();
+
+        for (int i=1; i<girilenSayi;i++){
+            if (girilenSayi%i==0){
+                toplam+=i;
+
+            }
+        }
+        if (girilenSayi==toplam){
+            System.out.println("Mükemmel sayıdır: "+girilenSayi);
+        }
+        if(girilenSayi!=toplam){
+            System.out.println("Mükemmel sayı değildir: "+girilenSayi);
+        }
+
+
+    }
+}
